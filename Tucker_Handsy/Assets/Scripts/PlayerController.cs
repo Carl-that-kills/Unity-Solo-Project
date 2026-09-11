35bb91073eee080c5b630a4b4796073be634fd40
Carl-that-kills <inderkiller620@gmail.com>
2026-09-11 13:44:23 -0700

    the starting setup for the player controls

--- Tucker_Handsy/Assets/Scripts/PlayerController.cs
+++ Tucker_Handsy/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumpHeight = 10.0f;
+
+    PlayerInput input;
+    Rigidbody rb;
+
+    public Vector2 moveInput = Vector2.zero;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
